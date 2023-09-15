c7e5d19bb18fb822915206dac2591048c7af54ea
Brij123987 <brijeshyadav9811@gmail.com>
2023-09-15 13:09:20 +0530

    commit:9

--- library/book/urls.py
+++ library/book/urls.py
@@ -3,5 +3,7 @@ from book import views
 
 urlpatterns = [
     path('home/', views.index, name='index'),
+    path('magazine/', views.magazine, name='magazine'),
+    path('comic/',views.comic, name='comic'),
     path('detail/', views.detail, name='detail'),
 ]
--- library/book/views.py
+++ library/book/views.py
@@ -1,13 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from book.models import Newspaper
+from book.models import Magazine
+from book.models import Comic
+
 
 # Create your views here.
 
 def index(request):
     itemlist = Newspaper.objects.all()
 
-    return HttpResponse(itemlist)
+    context = {
+        'itemlist':itemlist
+    }
+
+    return render(request, 'book/index.html', context)
+
+def magazine(request):
+    magazinelist = Magazine.objects.all()
+
+    context = {
+        'magazinelist':magazinelist
+    }
+
+    return render(request, 'book/magazine.html', context)
+
+def comic(request):
+    comiclist = Comic.objects.all()
+
+    context = {
+        'comiclist':comiclist
+    }
+
+    return render(request, 'book/comic.html', context)
 
 def detail(request):
     return HttpResponse('<h1 style="color:Blue">This is a Detail Page</h1>')
