1b1550606a54d26fe66d22348482ae2154f28735
Brij123987 <brijeshyadav9811@gmail.com>
2023-09-15 14:15:10 +0530

    commit:14

--- library/book/urls.py
+++ library/book/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from book import views
 
+app_name = 'book'
+
 urlpatterns = [
     path('home/', views.index, name='index'),
     path('magazine/', views.magazine, name='magazine'),
