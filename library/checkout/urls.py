7b95471f8852fe5383d5515eb41e7cfcad174a42
Brij123987 <134289082+Brij123987@users.noreply.github.com>
2023-12-22 14:41:51 +0530

    commit:44

--- library/checkout/urls.py
+++ library/checkout/urls.py
@@ -6,4 +6,6 @@ app_name = 'checkout'
 urlpatterns = [
     path('check/', views.chk_form, name='check'),
     path('check_page/',views.payment_page, name='check_page'),
+    path('on_approve/',views.OnApprove, name='on_approve'),
+    path('paymentsuccess/',views.PaymentSuccess, name='paymentsuccess'),
 ]
--- library/checkout/views.py
+++ library/checkout/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from checkout.models import Checkout_Detail
 from checkout.forms import Checkout_Forms
+from django.http import JsonResponse
+from book.context_processors import contact_form_context
+import json
+import datetime
 
 
 
@@ -26,6 +30,9 @@ def chk_form(request):
 
 def payment_page(request):
     check = Checkout_Detail.objects.all()
+    today = datetime.date.today()
+    now = datetime.datetime.now()
+    time = now.time()
     for val in check:
         name = val.name
         address = val.address
@@ -47,8 +54,56 @@ def payment_page(request):
         'order_id':order_id,
         'profile':profile,
         'item_data':item_data,
-        'total_price':total_price
+        'total_price':total_price,
+        'today':today,
+        'time':time
     }
 
     return render(request, 'checkout/checkout.html', context)
 
+
+
+def OnApprove(request):
+
+  if request.method == 'POST':
+    body = json.loads(request.body)
+    print(body)
+
+    context = {
+
+    }
+
+    return JsonResponse(context)
+
+
+
+def PaymentSuccess(request):
+    check = Checkout_Detail.objects.all()
+    today = datetime.date.today()
+    now = datetime.datetime.now()
+    time = now.time()
+    for val in check:
+        name = val.name
+        address = val.address
+        contact = val.contact_number
+        pin = val.pin_code
+        order_id = val.order_id
+    profile = request.user.email
+    item_data = request.session.get('item_data', [])
+
+    total_price = request.session.get('total_price', 0)
+    
+
+    context = {
+        'name':name,
+        'address':address,
+        'contact':contact,
+        'pin':pin,
+        'order_id':order_id,
+        'profile':profile,
+        'item_data':item_data,
+        'total_price':total_price,
+        'today':today,
+        'time':time
+    }
+    return render(request, 'checkout/paymentsuccess.html', context)
